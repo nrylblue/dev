e502b257da39eeb249cca166c812171dcb3d3018
nrylblue@local <nrylblue@local>
2017-11-16 22:18:11 -0800

    Another Checkin

--- dynamics/src/demo/test_demo.java
+++ dynamics/src/demo/test_demo.java
@@ -3,8 +3,8 @@ package demo;
 public class test_demo {
 
 	public static void main(String[] args) {
-		System.out.println("url"); 
-
+		System.out.println("url");
+		System.out.println("nourl");
 	}
 
 }
